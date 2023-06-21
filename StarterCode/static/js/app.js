a2fa0e5e7ec3cde281f1adc457f5a6a216e9e980
ava-viva <53435881+ava-viva@users.noreply.github.com>
2023-06-21 12:38:36 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,102 +1,115 @@
 // Using the URL for the data
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-const dataPromise = d3.json(url);
-    
-// getting the variables to use for the charts 
-var samples;
-var meta_data;
+const url =
+  "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// getting the variables to use for the charts
+let samples;
+let meta_data;
+
 d3.json(url).then(function (data) {
-    let selector = d3.select("#selDataset");
-    meta_data = data.metadata;
-    samples = data.samples;
-    data.names.forEach((id) => {
-        selector.append("option").text(id).property("value", id);
-    });
-    metaData(meta_data[0]);
-    horizonChart(samples[0]);
-    bubbleChart(samples[0]);
+  let selector = d3.select("#selDataset");
+  meta_data = data.metadata;
+  samples = data.samples;
+
+  data.names.forEach((id) => {
+    selector.append("option").text(id).property("value", id);
+  });
+
+  metaData(meta_data[0]);
+  horizonChart(samples[0]);
+  bubbleChart(samples[0]);
+});
+
+// Event listener for dropdown selection change
+d3.select("#selDataset").on("change", function () {
+  const selectedValue = d3.select(this).property("value");
+  itemSelecting(selectedValue);
 });
 
 function itemSelecting(value) {
-    const selectedId = samples.find((item) => item.id === value);
-    const demographicInfo = meta_data.find((item) => item.id == value);
-    metaData(demographicInfo);
-    horizonChart(selectedId);
-    bubbleChart(selectedId);
-
+  const selectedId = samples.filter((item) => item.id === value)[0];
+  const demographicInfo = meta_data.filter((item) => item.id == value)[0];
+  metaData(demographicInfo);
+  horizonChart(selectedId);
+  bubbleChart(selectedId);
 }
+
 // Display each key-value pair from the metadata JSON object somewhere on the page
 function metaData(demographicInfo) {
-    let demoSelect = d3.select("#sample-metadata");
+  let demoSelect = d3.select("#sample-metadata");
 
-    demoSelect.html(
-        `id: ${demographicInfo.id} <br> 
-      ethnicity: ${demographicInfo.ethnicity} <br>
+  demoSelect.html(
+    `id: ${demographicInfo.id} <br> 
+    ethnicity: ${demographicInfo.ethnicity} <br>
     gender: ${demographicInfo.gender} <br>
     age: ${demographicInfo.age} <br>
     location: ${demographicInfo.location} <br>
     bbtype: ${demographicInfo.bbtype} <br>
     wfreq: ${demographicInfo.wfreq}`
-    );
+  );
 }
-// Create a horizontal chart 
+
+// Create a horizontal chart
 function horizonChart(selectedId) {
-    let x_axis = selectedId.sample_values.slice(0, 10).reverse();
-    let y_axis = selectedId.otu_ids
-        .slice(0, 10)
-        .reverse()
-        .map((item) => `OTU ${item}`);
-    let text = selectedId.otu_labels.slice(0, 10).reverse();
+  let x_axis = selectedId.sample_values.slice(0, 10).reverse();
+  let y_axis = selectedId.otu_ids
+    .slice(0, 10)
+    .reverse()
+    .map((item) => `OTU ${item}`);
+  let text = selectedId.otu_labels.slice(0, 10).reverse();
 
-    barChart = {
-        x: x_axis,
-        y: y_axis,
-        text: text,
-        type: "bar",
-        orientation: "h",
-    };
+  const barChart = {
+    x: x_axis,
+    y: y_axis,
+    text: text,
+    type: "bar",
+    orientation: "h",
+  };
 
-    let chart = [barChart];
+  let chart = [barChart];
 
-    let layout = {
-        margin: {
-            l: 100,
-            r: 100,
-            t: 0,
-            b: 100,
-        },
-        height: 500,
-        width: 600,
-    };
+  let layout = {
+    margin: {
+      l: 100,
+      r: 100,
+      t: 0,
+      b: 100,
+    },
+    height: 500,
+    width: 600,
+  };
 
-    Plotly.newPlot("bar", chart, layout);
+  Plotly.newPlot("bar", chart, layout);
 }
-/// Create a bubble chart that displays each sample.
+
+// Create a bubble chart that displays each sample.
 function bubbleChart(selectedId) {
-    let x_axis = selectedId.otu_ids;
-    let y_axis = selectedId.sample_values;
-    let marker_size = selectedId.sample_values;
-    let color = selectedId.otu_ids;
-    let text = selectedId.otu_labels;
+  let x_axis = selectedId.otu_ids;
+  let y_axis = selectedId.sample_values;
+  let marker_size = selectedId.sample_values;
+  let color = selectedId.otu_ids;
+  let text = selectedId.otu_labels;
 
-    bubble = {
-        x: x_axis,
-        y: y_axis,
-        text: text,
-        mode: "markers",
-        marker: {
-            color: color,
-            colorscale: "Pastel",
-            size: marker_size,
-        },
-        type: "scatter",
-    };
-    let chart = [bubble];
+  const bubble = {
+    x: x_axis,
+    y: y_axis,
+    text: text,
+    mode: "markers",
+    marker: {
+      color: color,
+      colorscale: "Pastel",
+      size: marker_size,
+    },
+    type: "scatter",
+  };
 
-    let layout = {
-        xaxis: {
-            title: { text: "OTU ID" },
-        },
-    };
-    Plotly.newPlot("bubble", chart, layout);
+  let chart = [bubble];
+
+  let layout = {
+    xaxis: {
+      title: { text: "OTU ID" },
+    },
+  };
+
+  Plotly.newPlot("bubble", chart, layout);
 }
